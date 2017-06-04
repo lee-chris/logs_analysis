80af4e6edb8d10476d60b6efccfc64460c7125d4
Chris Lee <lee.christopher.e@gmail.com>
2017-06-04 14:49:16 -0700

    fixed formatting to conform to PEP8 style

--- logs_analysis.py
+++ logs_analysis.py
@@ -2,18 +2,19 @@
 
 import psycopg2
 
+
 def get_connection_info():
     """Get connection information from user input."""
-    
+
     user = input("Enter username: ")
     password = input("Enter password: ")
-    
+
     return user, password
 
-    
+
 def print_top_articles(cursor):
     """Print the 3 most popular articles."""
-    
+
     cursor.execute("""
         select title, cnt from (
             select title,
@@ -26,18 +27,18 @@ def print_top_articles(cursor):
         ) x
         limit 3
         """)
-    
+
     results = cursor.fetchall()
-    
+
     print("\nMost popular articles:\n")
-    
+
     for title, count in results:
         print("\"{0}\" - {1} views".format(title, count))
 
 
 def print_top_authors(cursor):
     """Print the 3 most popular authors."""
-    
+
     cursor.execute("""
         select name, cnt from (
             select aut.name,
@@ -52,23 +53,24 @@ def print_top_authors(cursor):
         ) x
         limit 3
         """)
-    
+
     results = cursor.fetchall()
-    
+
     print("\nMost popular authors:\n")
-    
+
     for name, count in results:
         print("{0} - {1} views".format(name, count))
 
 
 def print_days_with_error(cursor):
     """Print the days that had more than 1% error."""
-    
+
     cursor.execute("""
         select day, cnt_error / total
         from (
             select date_trunc('day', time) as day,
-                   sum(case when status <> '200 OK' then 1 else 0 end) as cnt_error,
+                   sum(case when status <> '200 OK' then 1
+                            else 0 end) as cnt_error,
                    count(*) * 1.0 as total
               from log
             group by day
@@ -76,14 +78,14 @@ def print_days_with_error(cursor):
         where cnt_error / total > 0.01
         order by day
         """)
-    
+
     results = cursor.fetchall()
-    
+
     print("\nDays with high amount of error:\n")
-    
+
     if (len(results) == 0):
         print("None")
-    
+
     else:
         for day, percent in results:
             print("{0:{dfmt}} - {1:.{prec}}% errors"
@@ -91,16 +93,17 @@ def print_days_with_error(cursor):
 
 
 def main():
-    
+
     user, password = get_connection_info()
-    conn = psycopg2.connect("dbname=news user={0} password={1}".format(user, password))
-    
+    conn = psycopg2.connect("dbname=news user={0} password={1}"
+                            .format(user, password))
+
     cursor = conn.cursor()
-    
+
     print_top_articles(cursor)
     print_top_authors(cursor)
     print_days_with_error(cursor)
-    
+
     conn.close()
 
 
